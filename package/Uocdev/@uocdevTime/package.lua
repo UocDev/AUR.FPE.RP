fbf6130111d515f066de08fcbf90a1943e8c3837
Uoc Developer <uocdev.github.io@gmail.com>
2025-06-26 16:09:57 +0800

    Create package.lua

---
+++ package/Uocdev/@uocdevTime/package.lua
@@ -0,0 +1,11 @@
+return {
+    name = "@uocdev/Time",
+    version = "1.0.0",
+    description = "",
+    author = "r5bcn2", 
+    license = "MIT",
+    repository = "https://github.com/UocDev/AUR.FPE.RP",
+    dependencies = {},
+    entry = "dist/index.lua",
+    tags = {""},
+}
