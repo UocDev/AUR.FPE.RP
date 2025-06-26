5d9759e8fe839534bfe7d4fdd1aaa0c7f47cfe03
Uoc Developer <uocdev.github.io@gmail.com>
2025-06-26 16:43:37 +0800

    Update package.lua

--- package/Uocdev/@uocdevTime/package.lua
+++ package/Uocdev/@uocdevTime/package.lua
@@ -1,11 +1,11 @@
 return {
     name = "@uocdev/Time",
     version = "1.0.0",
-    description = "",
+    description = "Meow",
     author = "r5bcn2", 
     license = "MIT",
     repository = "https://github.com/UocDev/AUR.FPE.RP",
     dependencies = {},
     entry = "dist/index.lua",
-    tags = {""},
+    tags = {"GUI, FPR"},
 }
